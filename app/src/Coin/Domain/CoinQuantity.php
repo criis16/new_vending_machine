2669a06d5888d935248cbaa28048cf7ea0c5ed24
criis16 <cristian.ayala96@gmail.com>
2026-09-09 00:32:55 +0200

    Add create method to CoinQuantity for validation of negative quantities

--- app/src/Coin/Domain/CoinQuantity.php
+++ app/src/Coin/Domain/CoinQuantity.php
@@ -2,6 +2,7 @@
 
 namespace App\Coin\Domain;
 
+use App\Coin\Domain\Exceptions\CoinWithNegativeQuantity;
 use App\Shared\Domain\ValueObject\IntQuantityValueObject;
 
 final class CoinQuantity extends IntQuantityValueObject
@@ -9,6 +10,17 @@ final class CoinQuantity extends IntQuantityValueObject
     public static function initialize(): self
     {
         return new self(1);
+    }
+
+    public static function create(int $quantity): self
+    {
+        $coinQuantity = new self($quantity);
+
+        if ($coinQuantity->isNegativeValue()) {
+            throw new CoinWithNegativeQuantity();
+        }
+
+        return $coinQuantity;
     }
 
     public function increment(): self
